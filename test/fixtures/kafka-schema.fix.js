a185da3eb47d5ecb945e5199a9c1d120481627ba
Thanasis Polychronakis <thanpolas@gmail.com>
2016-12-20 16:10:06 +0200

    create schema fixture for testing

---
+++ test/fixtures/kafka-schema.fix.js
@@ -0,0 +1,9 @@
+module.exports = {
+  'type' : 'record',
+  'name' : 'NodeKafkaTestSchema',
+  'namespace' : 'waldo.v1.event.core',
+  'fields' : [ {
+    'name' : 'foo',
+    'type' : 'string',
+  } ]
+};
