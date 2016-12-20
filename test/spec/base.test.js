282973a7b8fdba01c46826624a9996cc752f2881
Thanasis Polychronakis <thanpolas@gmail.com>
2016-12-20 14:28:16 +0200

    tweak test

--- test/spec/base.test.js
+++ test/spec/base.test.js
@@ -4,11 +4,11 @@
 const chai = require('chai');
 const expect = chai.expect;
 
-const kafka = require('../..');
+const kafkaLib = require('../..');
 
 describe('Base API Surface', function() {
   it('should expose expected methods', function(){
-    expect(kafka).to.have.keys([
+    expect(kafkaLib).to.have.keys([
       'Consumer',
     ]);
   });
