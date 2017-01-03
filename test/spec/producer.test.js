172b61dcca6a08251a63b92f35b1f78c190a6c1e
Thanasis Polychronakis <thanpolas@gmail.com>
2017-01-03 17:25:29 +0200

    reposition producer nominal test

--- test/spec/producer.test.js
+++ test/spec/producer.test.js
@@ -16,6 +16,34 @@ describe('Producer tests', function() {
     kafkaConnect.kafka = null;
   });
 
+  describe('Nominal behaviors', function() {
+    it('should produce successfully', function () {
+      kafkaLib.setKafkaUrl('http://localhost:8082');
+
+      const topic = 'noda-kafka-rest-test-success';
+
+      const producer = new kafkaLib.Producer({
+        topic: topic,
+        log: tester.log,
+        keySchema: { type: 'string' },
+        schema: schemaFix,
+        retryTimes: 5,
+        retryInterval: 300,
+      });
+
+      return producer.produce({
+        key: 'success',
+        value: {
+          foo: 'bar'
+        }
+      }).then(res => {
+        expect(res.offsets.length).to.equal(1);
+      }).catch(err => {
+        throw err;
+      });
+    });
+  });
+
   describe('Erroneous behaviors', function() {
     it('Should cope when kafka is not reachable but later becomes', function() {
       kafkaLib.setKafkaUrl('http://localhost:6666');
@@ -88,32 +116,5 @@ describe('Producer tests', function() {
           expect(err.source.message).to.equal('connect ECONNREFUSED 127.0.0.1:6666');
         });
     });
-
-    it('should produce successfully', function () {
-      kafkaLib.setKafkaUrl('http://localhost:8082');
-
-      const topic = 'noda-kafka-rest-test-success';
-
-      const producer = new kafkaLib.Producer({
-        topic: topic,
-        log: tester.log,
-        keySchema: { type: 'string' },
-        schema: schemaFix,
-        retryTimes: 5,
-        retryInterval: 300,
-      });
-
-      return producer.produce({
-        key: 'success',
-        value: {
-          foo: 'bar'
-        }
-      }).then(res => {
-        expect(res.offsets.length).to.equal(1);
-      }).catch(err => {
-        throw err
-      });
-    });
   });
-
 });
